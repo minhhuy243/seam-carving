2ed6fadac1b6c4a4406bc0513185b0bfb1358618
Phan Phi Hung <hungphan@hitek.com.vn>
2023-04-27 23:51:49 +0700

    refactor source & handle read and write file PPM

---
+++ main.cu
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "./src/host.h"
+
+using namespace std;
+
+int WIDTH;
+__device__ int d_WIDTH;
+
+int xSobel[3][3] = {{1,0,-1},{2,0,-2},{1,0,-1}};
+int ySobel[3][3] = {{1,2,1},{0,0,0},{-1,-2,-1}};
+__constant__ int d_xSobel[9] = {1, 0, -1, 2, 0, -2, 1, 0, -1};
+__constant__ int d_ySobel[9] = {1, 2, 1, 0, 0, 0, -1, -2, -1};
+const int filterWidth = 3;
---
+++ src/host.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define CHECK(call)\
+{\
+    const cudaError_t error = call;\
+    if (error != cudaSuccess)\
+    {\
+        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);\
+        fprintf(stderr, "code: %d, reason: %s\n", error,\
+                cudaGetErrorString(error));\
+        exit(EXIT_FAILURE);\
+    }\
+}
+void readPnm(char * fileName, int &width, int &height, uchar3 * &pixels);
+void writePnm(uchar3 *pixels, int width, int height, int originalWidth, char *fileName);
+
